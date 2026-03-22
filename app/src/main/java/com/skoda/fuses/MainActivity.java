476d945e76fa1508a4e617fe0a179c1a26dc1d1e
Zhursat <163448543+Zhursat@users.noreply.github.com>
2026-03-22 14:48:36 +0200

    Update MainActivity.java

--- app/src/main/java/com/skoda/fuses/MainActivity.java
+++ app/src/main/java/com/skoda/fuses/MainActivity.java
@@ -2,6 +2,7 @@ package com.skoda.fuses;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -14,7 +15,12 @@ public class MainActivity extends Activity {
         WebView webView = new WebView(this);
         setContentView(webView);
 
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
         webView.setWebViewClient(new WebViewClient());
-        webView.loadData("<h1>OK</h1>", "text/html", "utf-8");
+
+        webView.loadUrl("file:///android_asset/index.html");
     }
 }
